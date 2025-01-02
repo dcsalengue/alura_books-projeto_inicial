d91ae504b81af8eea539db2b723d073eba933145
dcsalengue <dcsalengue@me.com>
2025-01-02 17:09:03 -0300

    Aula 1

--- app/main.js
+++ app/main.js
@@ -5,21 +5,29 @@ const endpointDaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.js
 const elementoParaInserirLivros = document.getElementById("livros");
 async function getBuscarLivrosDaAPI(params) {
     const res = await fetch(endpointDaAPI);
-    const livros = await res.json();
-    console.table(livros);
-    livros.forEach((livro) => {
+    const listaDeLivros = await res.json();
+    console.table(listaDeLivros);
+
+    exibirOsLivrosNaTela(listaDeLivros);
+
+    
+    
+}
+
+function exibirOsLivrosNaTela(listaDeLivros) {
+    listaDeLivros.forEach((livro) => {
         const divLivro = document.createElement("div");
 
-        divLivro.innerHTML = `
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
-        <h2 class="livro__titulo">${livro.titulo}</h2>
-        <p class="livro__descricao">>${livro.autor}</p>
-        <p class="livro__preco" id="preco">${livro.preco}</p>
-        <div class="tags">
-            <span class="tag">${livro.categoria}</span>
+        elementoParaInserirLivros.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+            <h2 class="livro__titulo">${livro.titulo}</h2>
+            <p class="livro__descricao">>${livro.autor}</p>
+            <p class="livro__preco" id="preco">R$${livro.preco.toFixed(2)}</p>
+            <div class="tags">
+                <span class="tag">${livro.categoria}</span>
+            </div>
         </div>
         `
-        elementoParaInserirLivros.appendChild(divLivro);
     });
-    
-} 
+}
