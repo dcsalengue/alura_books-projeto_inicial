93daca65053d8f1b8b4a8a747637b694a7d62939
dcsalengue <dcsalengue@me.com>
2025-01-02 15:32:19 -0300

    Aula1 - 6 inserindo livros (antes de olhar a solução do vídeo)

---
+++ app/main.js
@@ -0,0 +1,25 @@
+let livros = [];
+const endpointDaAPI = "https://guilhermeonrails.github.io/casadocodigo/livros.json";
+ getBuscarLivrosDaAPI();
+
+const elementoParaInserirLivros = document.getElementById("livros");
+async function getBuscarLivrosDaAPI(params) {
+    const res = await fetch(endpointDaAPI);
+    const livros = await res.json();
+    console.table(livros);
+    livros.forEach((livro) => {
+        const divLivro = document.createElement("div");
+
+        divLivro.innerHTML = `
+        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}" />
+        <h2 class="livro__titulo">${livro.titulo}</h2>
+        <p class="livro__descricao">>${livro.autor}</p>
+        <p class="livro__preco" id="preco">${livro.preco}</p>
+        <div class="tags">
+            <span class="tag">${livro.categoria}</span>
+        </div>
+        `
+        elementoParaInserirLivros.appendChild(divLivro);
+    });
+    
+} 
