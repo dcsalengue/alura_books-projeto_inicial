6b9cba1aecd5ef2ef15577bb3222485f55fa1da8
dcsalengue <dcsalengue@me.com>
2025-01-02 23:52:53 -0300

    Aula 4 - Sort

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,12 +1,15 @@
 
 const botoes = document.querySelectorAll(".btn")
 
-botoes.forEach(btn => {
-    btn.addEventListener("click", () => {
-        let livrosFiltrados =  livros.filter(livro => livro.categoria === btn.value)  
-        console.table(livrosFiltrados)
-    })
-});
+botoes.forEach(btn => btn.addEventListener("click", filtrarLivros));
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id);
+    const categoria = elementoBtn.value
+    let livrosFiltrados = categoria == 'disponivel' ? livros.filter(livro => livro.quantidade > 0) : livros.filter(livro => livro.categoria === categoria)
+    console.table(livrosFiltrados)
+    exibirOsLivrosNaTela(livrosFiltrados);
+
+}
 
 
 
